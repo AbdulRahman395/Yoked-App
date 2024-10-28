6cce7d4a7450bb7260044607094cd4f70dc1d885
AbdulRahman395 <arahmanonline937@gmail.com>
2024-10-28 16:56:42 +0500

    .

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -4,15 +4,15 @@ const chatController = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddleware')
 
 // Route to load the dashboard (make sure the user is logged in)
-user_route.get('/dashboard', authMiddleware, chatController.loadDashboard);
+router.get('/dashboard', authMiddleware, chatController.loadDashboard);
 
 // Route to save a new chat message
-user_route.post('/save-chat', authMiddleware, chatController.saveChat);
+router.post('/save-chat', authMiddleware, chatController.saveChat);
 
 // Route to delete a chat message
-user_route.post('/delete-chat', authMiddleware, chatController.deleteChat);
+router.post('/delete-chat', authMiddleware, chatController.deleteChat);
 
 // Route to update a chat message
-user_route.post('/update-chat', authMiddleware, chatController.updateChat);
+router.post('/update-chat', authMiddleware, chatController.updateChat);
 
 module.exports = router;
