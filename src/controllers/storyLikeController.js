37d6cfca3ccfde96440a2b7f2bf807496debaef2
AbdulRahman395 <166097156+AbdulRahman395@users.noreply.github.com>
2024-10-30 14:30:15 +0500

    .

--- src/controllers/storyLikeController.js
+++ src/controllers/storyLikeController.js
@@ -1,6 +1,6 @@
 const StoryLike = require('../models/StoryLike');
 const Story = require('../models/Story');
-const User = require('../models/User');
+const Notification = require('../models/Notification');
 
 const storyLikeController = {
     // When user likes story, other user should be notified... and should also be display likes of the stories while user hits getMyStory API
@@ -8,7 +8,7 @@ const storyLikeController = {
         try {
             const { storyId } = req.params;
             const userId = req.user._id;
-
+    
             // Check if the user already likes the story
             const existingLike = await StoryLike.findOne({ storyId, userId });
             if (existingLike) {
@@ -17,18 +17,30 @@ const storyLikeController = {
                     message: "You have already liked this story"
                 });
             }
-
+    
             // Create a new like
             const like = new StoryLike({ storyId, userId });
             await like.save();
-
+    
             // Update the story's like count in the Story model
             const updatedStory = await Story.findByIdAndUpdate(
                 storyId,
                 { $inc: { likesCount: 1 } },
                 { new: true }
             );
-
+    
+            // Notify the story owner
+            const storyOwner = updatedStory.userId;
+            if (storyOwner.toString() !== userId.toString()) {
+                const notification = new Notification({
+                    userId: storyOwner,
+                    title: "New Story Like",
+                    message: `Your story was liked by a user.`,
+                    isRead: false
+                });
+                await notification.save();
+            }
+    
             res.status(200).json({
                 success: true,
                 message: "Story liked successfully",
@@ -42,7 +54,7 @@ const storyLikeController = {
                 error: error.message
             });
         }
-    }
+    }    
 
 }
 
