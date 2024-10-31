eb9822086d40a8439c5c7668c6e9657130f27d93
AbdulRahman395 <166097156+AbdulRahman395@users.noreply.github.com>
2024-10-31 16:48:08 +0500

    .

---
+++ src/models/Contact.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const contactUsSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    match: [/.+\@.+\..+/, 'Please enter a valid email address'],
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('ContactUs', contactUsSchema);
